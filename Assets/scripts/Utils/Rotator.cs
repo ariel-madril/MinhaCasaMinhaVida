b9631cda64d8cbe5f42a9b1dc93c71048250fd04
tota5 <tota5forever@gmail.com>
2018-12-19 02:49:29 -0400

    more stuff

---
+++ Assets/scripts/Utils/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_RotationToCopy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = m_RotationToCopy.rotation;
+    }
+}
