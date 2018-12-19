3154df09bba0ab4837dd33f08efdf7ee9931a82c
tota5 <tota5forever@gmail.com>
2018-12-19 09:05:55 -0400

    adding light bulbs

--- Assets/scripts/Lights/LightSwitch.cs
+++ Assets/scripts/Lights/LightSwitch.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Grabbable))]
+[RequireComponent(typeof(GrabbableObject))]
 public class LightSwitch : MonoBehaviour
 {
     [SerializeField]
     List<LightBulb> m_LigtsToSwitch;
 
-    Grabbable m_Grabbable;
+    GrabbableObject m_Grabbable;
 
     bool m_SwitchState = false;
 
     private void Start()
     {
-        m_Grabbable = GetComponent<Grabbable>();
-        m_Grabbable.GrabbedEvents.AddListener(Interacted);
     }
 
-    void Interacted()
+    public void Interacted()
     {
         m_SwitchState = !m_SwitchState;
         foreach(LightBulb bulb in m_LigtsToSwitch)
